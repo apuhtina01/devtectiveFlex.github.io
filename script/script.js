7b808636e81b75a5c37bd1a600fdb835da118b1b
DevtectiveFlex <148262074+DevtectiveFlex@users.noreply.github.com>
2024-03-14 17:25:28 +0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function openSubmenu() {
+  var submenu = document.querySelector('.submenu');
+  submenu.classList.add('open'); // Добавляем класс для открытия блока
+}
+
+function closeSubmenu() {
+  var submenu = document.querySelector('.submenu');
+  submenu.classList.remove('open'); // Удаляем класс для закрытия блока
+}
