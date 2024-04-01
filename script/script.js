58b81a1538a94b23388362ec5492c907b216a930
DevtectiveFlex <148262074+DevtectiveFlex@users.noreply.github.com>
2024-04-01 17:28:02 +0300

    add  interactive zoom for marker items

--- script/script.js
+++ script/script.js
@@ -16,6 +16,8 @@ let mapCustomization = await getFile('./script/customization.json')
 let mapBaloons = await getFile('./script/map.json')
 let cities = await getFile('./script/cities.json')
 let cityItems = document.querySelectorAll('.city-item')
+let markersList = document.querySelectorAll('.marker__item')
+let isMoscow = false
 
 initMap();
 
@@ -28,6 +30,41 @@ async function initMap() {
   const { YMapClusterer, clusterByGrid } = await ymaps3.import('@yandex/ymaps3-clusterer@0.0.1');
   const { YMapDefaultMarker } = await ymaps3.import('@yandex/ymaps3-markers@0.0.1');
   // Иницилиазируем карту
+  function createMarkerItem(marker) {
+    let markerItem = document.createElement('li')
+    markerItem.classList.add('marker__item')
+
+
+    let title = document.createElement('h2')
+    title.classList.add("marker__title")
+    title.textContent = marker.title
+
+    let subtitle = document.createElement('p')
+    subtitle.classList.add("marker__subtitle")
+    subtitle.textContent = marker.subtitle
+
+    let address = document.createElement('p')
+    address.classList.add("marker__address")
+    address.textContent = marker.address
+
+    let phone = document.createElement('p')
+    phone.classList.add("marker__phone")
+    phone.textContent = marker.phone
+
+    let markerCoordinates = document.createElement('p')
+    markerCoordinates.classList.add("marker__coordinates")
+    markerCoordinates.textContent = marker.coordinates
+
+    markerItem.append(title)
+    markerItem.append(subtitle)
+    markerItem.append(address)
+    markerItem.append(phone)
+    markerItem.append(markerCoordinates)
+    document.getElementById('markerList').append(markerItem)
+    markerItem.onclick = function () {
+      map.update({ location: { center: marker.coordinates, zoom: 18 } });
+    }
+  }
   const map = new YMap(
     // Передаём ссылку на HTMLElement контейнера
     document.getElementById('map'),
@@ -47,60 +84,41 @@ async function initMap() {
   // Добавляем слой для отображения схематической карты
   map.addChild(new YMapDefaultSchemeLayer({ customization: mapCustomization }))
   map.addChild(new YMapDefaultFeaturesLayer({}))
+  for (let item of cityItems) {
+    item.onclick = function () {
+      let city = cities.find(function (city) {
+        return city.id == item.id
+      })
+      let cityMarkers = city.markers
+      document.getElementById('markerList').innerHTML = '';
+      for (let marker of cityMarkers) {
+        createMarkerItem(marker)
 
-  for (let item of cityItems){
-   item.onclick = function (){
-   let city = cities.find(function (city){
-      return city.id == item.id
-    })
-    let cityMarkers = city.markers
-    document.getElementById('markerList').innerHTML = '';
-    for (let marker of cityMarkers) {
-      let markerItem = document.createElement('li')
-      markerItem.classList.add('marker__item')
-
-
-      let title = document.createElement('h2')
-      title.classList.add("marker__title")
-      title.textContent = marker.title
-
-     let subtitle = document.createElement('p')
-    subtitle.classList.add("marker__subtitle")
-    subtitle.textContent = marker.subtitle
-
-    let address = document.createElement('p')
-    address.classList.add("marker__address")
-    address.textContent = marker.address
-
-    let phone = document.createElement('p')
-    phone.classList.add("marker__phone")
-    phone.textContent = marker.phone
-
-    markerItem.append(title)
-    markerItem.append(subtitle)
-    markerItem.append(address)
-    markerItem.append(phone)
-    document.getElementById('markerList').append(markerItem)
+      }
+      map.update({ location: { center: city.coordinates, zoom: 10 } });
+      document.getElementById('map').scrollIntoView({ block: "center", behavior: "smooth" })
     }
-    map.update({location: {center: city.coordinates, zoom: 10}});
-    document.getElementById('map').scrollIntoView ({ block: "center", behavior: "smooth" })
-   }
   }
   // Add a default marker with a popup window from the package to the map
   for (let item of cities) {
+
     let city = item.markers
-      for (let marker of city){
-    map.addChild(
-      new YMapDefaultMarker({
-        coordinates: marker.coordinates,
-        color: marker.color,
-        title: marker.title,
-        subtitle: marker.subtitle,
-        popup: { content: marker.popup.content, position: marker.popup.position }
-      })
-    )
+    if (item.id == 'msk') { isMoscow = true } else (isMoscow = false)
+
+    for (let marker of city) {
+      if (isMoscow) {
+        createMarkerItem(marker)
+      }
+      map.addChild(
+        new YMapDefaultMarker({
+          coordinates: marker.coordinates,
+          color: marker.color,
+          title: marker.title,
+          subtitle: marker.subtitle,
+          popup: { content: marker.popup.content, position: marker.popup.position }
+        })
+      )
     }
   }
-
 }
 
